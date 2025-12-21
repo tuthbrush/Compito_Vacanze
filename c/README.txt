1162292abdacba26e5f28bfd101dfb22bf89c0a0
tuthbrush <principericconigeriano@gmail.com>
2025-12-21 11:25:28 +0100

    non ricordo

--- c/README.txt
+++ c/README.txt
@@ -6,3 +6,21 @@ Cosa ho fatto il 19/12/25:
 4- Swicth case per impostare la difficoltà | TESTATA E FUNZIONANTE
 5- abbozzata la logica del movimento | TESTATA E FUNZIONANTE
 
+
+void loading(){
+    char R[10][2] = {" "," "," "," "," "," "," "," "," "," "};
+    int loadval = 0;
+    for(int i = 0; i!=10; i++){
+        R[i][0] = '#';
+        loadval+=10;
+        printf("[");
+        for(int c = 0; c!=10; c++){
+            printf("%c", R[c][0], loadval);
+        }
+        printf("] %d%%", loadval);
+        fflush(stdout);
+        usleep((rand()%(850000-200000))+200000);
+        printf("\r");
+    }
+    printf("\n");
+}
--- c/main.c
+++ c/main.c
@@ -4,6 +4,17 @@
 #include <string.h>
 #include <unistd.h>
 #include "lib.h"
+
+#define AC_BLACK "\x1b[30m"
+#define AC_RED "\x1b[31m"
+#define AC_GREEN "\x1b[32m"
+#define AC_YELLOW "\x1b[33m"
+#define AC_BLUE "\x1b[34m"
+#define AC_MAGENTA "\x1b[35m"
+#define AC_CYAN "\x1b[36m"
+#define AC_WHITE "\x1b[37m"
+#define AC_NORMAL "\x1b[m"
+
 // Variabili 
 
 #define PROPN 24 // Numero proprietà 
@@ -13,14 +24,16 @@ int scelta;
 float contoBancario = 0;
 char word[22];
 char proprieties[PROPN][22] = {"Start\0","Societa Elettrica'\0","Piazza Universita'\0","Viale Monterosa\0","Corso Magellano\0","Viale Traiano\0",
-                            "Via Roma\0","Stazione Nord\0","Piazza Giulio Cesare\0","Via Verdi\0","Viale dei Giardini\0","Viale Liberta'\0",
-                            "Via Onomatopea\0","Stazione Ovest\0","Palazzo della regione\0",
-                            "Via Rossini\0","Corso San Maurizio\0","Via Tarino","Gobetti\0","Mole\0","Stazione Est\0","Giardini reali\0",
-"Parco della Vittoria\0","Avogadro\0"}; // Matrice per definire le proprietà
+                                "Via Roma\0","Stazione Nord\0","Piazza Giulio Cesare\0","Via Verdi\0","Viale dei Giardini\0","Viale Liberta'\0",
+                                "Via Onomatopea\0","Stazione Ovest\0","Palazzo della regione\0",
+                                "Via Rossini\0","Corso San Maurizio\0","Via Tarino","Gobetti\0","Mole\0","Stazione Est\0","Giardini reali\0",
+                                "Parco della Vittoria\0","Avogadro\0"}; // Matrice per definire le proprietà
 
 int prices[] = {0,350,150,600,50,800,1000,1200,700,850,5400,2200,40,1200,4500,1400,6000,400,2300,8000,1200,4600,7200,10000}; // Da definire
 
-int numCase[PROPN] = {1,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,2,1,0,2,0,0,0,0,}; //to modify
+int rents[] = {}; // Da definire
+
+int numCase[PROPN] = {1,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,2,1,0,2,0,0,0,0,}; // to modify
 
 int cellaAttuale = 0; // cella attuale del player
 
@@ -131,8 +144,7 @@ int main()
             int nuovaPosizione = (cellaAttuale + dado);
             cellaAttuale = nuovaPosizione;
         }
-        
-
+        pulisci();
     } while (contoBancario<target);
     
     
